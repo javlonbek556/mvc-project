f88963cc9cee3b6088757899fdefca6912723c11
javlonbek556 <javlonzdess@gmail.com>
2025-02-19 16:52:15 +0500

    this codes enterance form

---
+++ posts/create.php
@@ -0,0 +1,5 @@
+<form method="POST" action="/posts/create.php">
+    <input type="text" name="title" placeholder="Title" required>
+    <textarea name="content" placeholder="Content" required></textarea>
+    <button type="submit">Create Post</button>
+</form>
