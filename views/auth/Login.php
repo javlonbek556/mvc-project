36436515c198d5cbb9fa131e4cb02723768fd8d0
javlonbek556 <javlonzdess@gmail.com>
2025-02-19 16:54:30 +0500

    there are enterance form

---
+++ views/auth/Login.php
@@ -0,0 +1,5 @@
+<form method="POST" action="/login.php">
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+</form>
